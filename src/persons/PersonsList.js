2aeb61a108cede2cc7f9ae5c031be4df4b656a08
emanueljtc <emanueljtc@gmail.com>
2019-03-21 15:33:01 -0400

    mostrando data desde json

--- src/persons/PersonsList.js
+++ src/persons/PersonsList.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 // import './App.css';
+import PeopleData from '../data/persons.json'
 
 class PersonsList extends Component {
   render() {
     return (
       <div className="List">
         <h2>Lista de Personas</h2>
+        {PeopleData.map((peopleDetail, index) =>{
+          return (
+            <div>
+              <table className="PeopleList">
+                <tr>
+                  <th>LEAD</th>
+                  <th>EMAIL</th>
+                  <th>PHONE</th>
+                  <th>SOURCE</th>
+                  <th>STATUS</th>
+                  <th>OPTIONS</th>
+                </tr>
+                <tr>
+                  <td>{peopleDetail.name}<br />{peopleDetail.case}</td>
+                  <td>{peopleDetail.email}</td>
+                  <td>{peopleDetail.phone}</td>
+                  <td>{peopleDetail.source}</td>
+                  <td>{peopleDetail.status}</td>
+                  <td>...</td>
+                </tr>
+              </table>
+            </div>
+            )
+        })}
       </div>
     );
   }
