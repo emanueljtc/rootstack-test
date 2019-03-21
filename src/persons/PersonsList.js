f266646e50e5f9cdc4aaa82273050abe6703dd42
emanueljtc <emanueljtc@gmail.com>
2019-03-21 17:48:33 -0400

    refactor: table refactor

--- src/persons/PersonsList.js
+++ src/persons/PersonsList.js
@@ -6,30 +6,32 @@ class PersonsList extends Component {
   render() {
     return (
       <div className="people-list">
-        <h2>Lista de Personas</h2>
-          <table className="list">
-            <tr>
-              <th>LEAD</th>
-              <th>EMAIL</th>
-              <th>PHONE</th>
-              <th>SOURCE</th>
-              <th>STATUS</th>
-              <th>OPTIONS</th>
-            </tr>
-        {PeopleData.map((peopleDetail, index) =>{
-          return (
-                <tr>
-                  <td>{peopleDetail.name}<br />{peopleDetail.case}</td>
-                  <td>{peopleDetail.email}</td>
-                  <td>{peopleDetail.phone}</td>
-                  <td>{peopleDetail.source}</td>
-                  <td>{peopleDetail.status}</td>
-                  <td>...</td>
-                </tr>
-             
-            )
-        })}
-        </table>
+          <div className="list">
+            <span className="head lead-head"><strong>LEAD</strong></span>
+            <span className="head"><strong>EMAIL</strong></span>
+            <span className="head"><strong>PHONE</strong></span>
+            <span className="head"><strong>SOURCE</strong></span>
+            <span className="head"><strong></strong></span>
+            <span className="head"><strong></strong></span>
+            {PeopleData.map((peopleDetail, index) =>{
+              return (
+                <>
+                  <span className="lead">
+                    <img className="image-people" src={peopleDetail.img} />
+                    <div class="info">
+                      <strong><p className="name">{peopleDetail.name}</p></strong>
+                      <a href="#" className="case">Case: {peopleDetail.case}</a>
+                    </div>
+                  </span>
+                  <span><strong>{peopleDetail.email}</strong></span>
+                  <span><strong>{peopleDetail.phone}</strong></span>
+                  <span><strong>{peopleDetail.source}</strong></span>
+                  <span><strong>{peopleDetail.status}</strong></span>
+                  <span>...</span>
+                </>
+                )
+            })}
+        </div>
       </div>
     );
   }
