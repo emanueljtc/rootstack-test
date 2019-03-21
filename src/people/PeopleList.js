5d50e0b5a96267d12005c9ffa592e0f9e01e484d
emanueljtc <emanueljtc@gmail.com>
2019-03-21 14:53:28 -0400

    Feat: Creando Componente PeopleList

---
+++ src/people/PeopleList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class PeopleList extends Component {
+  render() {
+    return (
+      <div className="List">
+        <h2>Lista de Personas</h2>
+      </div>
+    );
+  }
+}
+
+export default PeopleList;
