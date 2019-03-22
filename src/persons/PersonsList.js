6054132eda574a116b9872c0228e3a56af0577d2
emanueljtc <emanueljtc@gmail.com>
2019-03-22 13:02:18 -0400

    refactor: new changes details and header

--- src/persons/PersonsList.js
+++ src/persons/PersonsList.js
@@ -16,6 +16,18 @@ class PersonsList extends Component {
             <input className="input-search" />
           </div>
           <div className="options-header">
+            <div className="type">
+              <span className="text">Type <i className="fas fa-angle-down"></i></span>
+            </div>
+            <div className="source">
+              <span className="text">Source <i className="fas fa-angle-down"></i></span>
+            </div>
+            <div className="content-button">
+              <button className="btn-add">
+                <i class="fas fa-plus"></i>  
+                Add Contact
+              </button>
+            </div>
           </div>
         </div>
           <div className="list">
@@ -56,7 +68,40 @@ class PersonsList extends Component {
                 )
             })}
         </div>
-      </div>
+        <div className="detail">
+          {PersonsData.map((personsDetail, index) => {
+            return (
+              <div className="person">
+                <span className="lead">
+                  <img className="image-people" src={personsDetail.image} />
+                  <div className="info">
+                    <strong><p className="name">{personsDetail.name}</p></strong>
+                    <a href="#" className="case">Case: {personsDetail.case}</a>
+                  </div>
+                </span>
+                <span className="email"><strong>{personsDetail.email}</strong></span>
+                <span className="phone"><strong>{personsDetail.phone}</strong></span>
+                <span className="source">
+                  <strong>
+                    {personsDetail.source}
+                    <i className="fas fa-angle-down"></i>
+                  </strong>
+                </span>
+                <span className="status">
+                  <div class="select">
+                    <div className="circle"></div>
+                    <strong>{personsDetail.status}</strong>
+                    <i className="fas fa-angle-down"></i>
+                  </div>
+                  <div className="options">
+                    ...
+                    </div>
+                </span>
+              </div>
+            )
+          })}
+        </div>
+        </div>
     );
   }
 }
