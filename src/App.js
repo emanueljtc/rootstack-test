a56114778220136d528f64f2b06d1ac28c7561c1
emanueljtc <emanueljtc@gmail.com>
2019-03-22 18:32:33 -0400

    feat: menu style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import PersonsList from './persons/PersonsList';
+import Menu from './menu/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="content-menu">
-          <div className="Menu">
-              Home
-          </div>
+          <Menu/>
         </div>
         <div className="content-list">
           <PersonsList/>
---
+++ src/menu/Menu.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import '../styles/Menu.css';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+        <div className="sidebar-menu">
+          <ul className="ul-content">
+            <li className="header-menu">
+              <i className="fas fa-bars"></i>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="fas fa-home"></i>
+                <span>Home</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="far fa-user-circle"></i>
+                <span>Leads</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="far fa-check-square"></i>
+                <span>Tasks</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="far fa-clone"></i>
+                <span>Forms</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="fas fa-user-plus"></i>
+                <span>Refferals</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="fas fa-tasks"></i>
+                <span>Checklists</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="far fa-calendar-alt"></i>
+                <span>Calendar</span>
+              </a>
+            </li>
+            <li className="sidebar-dropdown">
+              <a href="#">
+                <i className="fas fa-cog"></i>
+                <span>Settings</span>
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
