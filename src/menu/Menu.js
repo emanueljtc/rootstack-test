2e7b1c022c259cda158dbcc6c49a84022baff9e0
emanueljtc <emanueljtc@gmail.com>
2019-03-23 01:16:05 -0400

    fix waring href

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -11,49 +11,49 @@ class Menu extends Component {
               <i className="fas fa-align-right"></i>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="fas fa-home"></i>
                 <span>Home</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="far fa-user-circle"></i>
                 <span>Leads</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="far fa-check-square"></i>
                 <span>Tasks</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="far fa-clone"></i>
                 <span>Forms</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="fas fa-user-plus"></i>
                 <span>Refferals</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="fas fa-tasks"></i>
                 <span>Checklists</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="far fa-calendar-alt"></i>
                 <span>Calendar</span>
               </a>
             </li>
             <li className="sidebar-dropdown">
-              <a href="#">
+              <a href="/">
                 <i className="fas fa-cog"></i>
                 <span>Settings</span>
               </a>
